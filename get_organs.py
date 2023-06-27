b2f23865e90eb0e124535c373e40a5da2c19657c
hunterxtang <38674961+hunterxtang@users.noreply.github.com>
2023-06-27 15:10:21 -0400

    Update get_organs.py

--- get_organs.py
+++ get_organs.py
@@ -1,6 +1,8 @@
 # URL to call: http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//ref-organ-glb-files?endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql
 import os
 import requests
+import csv
+from urllib.parse import urlparse
 
 def main():
     # First, we call the CCF API with a SPARQL query, all via grlc.io: http://grlc.io/api/hubmapconsortium/ccf-grlc/ccf/#/default/get_ref_organ_glb_files
@@ -15,15 +17,26 @@ def main():
     save_path = "downloaded_organs/"
     os.makedirs(save_path, exist_ok=True)
     
-    for i in response:
-        glb_url = i['glb_url'].strip('"')
-        organ_name = i['scene_node'].strip('"')
-        glb_response = requests.get(glb_url)
-        if glb_response.status_code == 200:
-            file_path = os.path.join(save_path, f"{organ_name}.glb") 
-            with open(file_path, "wb") as file:
-                file.write(glb_response.content)
-                print(f"Downloaded {organ_name}.glb")
+    downloaded_url = []
+    csvp = "organ_files.csv"
+
+    with open(csvp,"w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["ref_organ", "scene_node", "URL"])
+
+        for i in response:
+            glb_url = i['glb_url'].strip('"')
+            organ_name = i['scene_node'].strip('"')
+            new_n = urlparse(glb_url)
+            file_name = os.path.splitext(os.path.basename(new_n.path))[0]
+            glb_response = requests.get(glb_url)
+            if glb_response.status_code == 200:
+                file_path = os.path.join(save_path, f"{file_name}.glb") 
+                with open(file_path, "wb") as file:
+                    file.write(glb_response.content)
+                    print(f"Downloaded {file_name}.glb")
+                    downloaded_url.append(glb_url)
+                    writer.writerow([i['ref_organ'].strip('"'),organ_name,glb_url])
     
 if __name__ == "__main__":
     main()
